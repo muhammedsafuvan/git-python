227ede496fd510316e4d513c52c8b9a0e6a3884a
muhammedsafuvan <muhammedsafuvan024@gmail.com>
2024-11-25 13:58:23 +0530

    create blob

--- app/main.py
+++ app/main.py
@@ -1,7 +1,49 @@
 import sys
 import os
 import zlib
+import hashlib 
 
+def init():
+    os.mkdir(".git")
+    os.mkdir(".git/objects")
+    os.mkdir(".git/refs")
+    with open(".git/HEAD", "w") as f:
+        f.write("ref: refs/heads/main\n")
+    print("Initialized git directory")
+
+def cat_file(sha):
+    path = f"./.git/objects/{sha[:2]}/{sha[2:]}"
+    with open(path, 'rb') as f:  # Open the file in binary mode
+        content = zlib.decompress(f.read())  # Read and decompress the binary data
+        # print(content)
+        null_byte_index = content.find(b'\x00')
+        if null_byte_index == -1:
+            raise ValueError("Invalid Git object: null byte not found")
+        
+        # Slice the content to extract the message
+        message = content[null_byte_index + 1:].strip(b"\n")
+        print(message.decode('utf-8'), end="")
+
+def hash_object(file):
+    with open(file, 'rb') as f:
+        content = f.read()
+
+        header = f"blob {len(content)}\x00"
+        store = header.encode("ascii") + content
+
+
+        sha = hashlib.sha1(store).hexdigest()
+        git_path = os.path.join(os.getcwd(), ".git/objects")
+        os.mkdir(os.path.join(git_path, sha[0:2]))
+        with open(os.path.join(git_path, sha[0:2], sha[2:]), "wb") as file:
+            file.write(zlib.compress(store))
+
+        print(sha, end="")
+
+        
+
+
+    
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,25 +53,18 @@ def main():
     #
     command = sys.argv[1]
     if command == "init":
-        os.mkdir(".git")
-        os.mkdir(".git/objects")
-        os.mkdir(".git/refs")
-        with open(".git/HEAD", "w") as f:
-            f.write("ref: refs/heads/main\n")
-        print("Initialized git directory")
+        init()
+
     elif command == "cat-file":
         sha = sys.argv[3]
-        path = f"./.git/objects/{sha[:2]}/{sha[2:]}"
-        with open(path, 'rb') as f:  # Open the file in binary mode
-            content = zlib.decompress(f.read())  # Read and decompress the binary data
-            # print(content)
-            null_byte_index = content.find(b'\x00')
-            if null_byte_index == -1:
-                raise ValueError("Invalid Git object: null byte not found")
-            
-            # Slice the content to extract the message
-            message = content[null_byte_index + 1:].strip(b"\n")
-            print(message.decode('utf-8'), end="")
+        cat_file(sha)
+
+    elif command == "hash-object":
+        if len(sys.argv) != 4:
+            exit()
+        
+        file = sys.argv[3]
+        hash_object(file)
 
     
 
