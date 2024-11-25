27b56b99bb365e4b55813b0fda7deb161d1e4903
muhammedsafuvan <muhammedsafuvan024@gmail.com>
2024-11-25 13:31:34 +0530

    read blob object

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import zlib
 
 
 def main():
@@ -16,6 +17,22 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    elif command == "cat-file":
+        sha = sys.argv[3]
+        path = f"./.git/objects/{sha[:2]}/{sha[2:]}"
+        with open(path, 'rb') as f:  # Open the file in binary mode
+            content = zlib.decompress(f.read())  # Read and decompress the binary data
+            # print(content)
+            null_byte_index = content.find(b'\x00')
+            if null_byte_index == -1:
+                raise ValueError("Invalid Git object: null byte not found")
+            
+            # Slice the content to extract the message
+            message = content[null_byte_index + 1:].strip(b"\n")
+            print(message.decode('utf-8'), end="")
+
+    
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
